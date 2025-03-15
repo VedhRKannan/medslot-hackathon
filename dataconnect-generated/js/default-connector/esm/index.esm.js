51e37e0e7d8457b508628986d22f9b4ead13662c
Ethan Chang <1905change@gmail.com>
2025-03-15 17:15:42 +0000

    Init

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'medslot',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'medslot',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
